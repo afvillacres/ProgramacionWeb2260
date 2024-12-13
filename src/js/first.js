1e693c73d6d3ffbe1d34fe1eacdc26cbbe01a6a1
afvillacres <afvillacres@espe.edu.ec>
2024-12-13 12:47:52 -0500

    Primeros pasos en JS

---
+++ src/js/first.js
@@ -0,0 +1,15 @@
+var nombre="Ricardo";
+var edad;
+edad=25;
+var apellido ="Vargas";
+console.log("Mi nombre es "+ nombre + " " + apellido);
+console.log("Mi edad es "+ edad);
+console.log("Mi nombre es "+ nombre + " " + apellido + " " + "y tengo "+ edad);
+
+let x =10;
+let y =20;
+let z = x + y;
+console.log(z);
+
+//var es una variable globa
+//let declara 
